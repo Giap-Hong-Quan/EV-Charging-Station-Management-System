8e2b3558f8e11e2bb5b482d14085c425156b9b9c
Giap-Hong-Quan <giaphongquan2407@gmail.com>
2025-10-14 00:38:04 +0700

    charging point api

--- Station_Management_Service/src/server.js
+++ Station_Management_Service/src/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import stationRoutes from './routes/StationRouter.js'
+import ChargingPointRoutes from './routes/ChargingPointRouter.js'
 import { connectDB } from './config/db.js';
 import cors from "cors";
 const app =express();// gọi thư viện 
 dotenv.config(); // nạp file .env
 app.use(express.json());
 app.use(cors());
-app.use('/station',stationRoutes)
+app.use('/station',stationRoutes);
+app.use('/chargingPoint',ChargingPointRoutes);
 const port=process.env.PORT
 
 connectDB().then(()=>{
