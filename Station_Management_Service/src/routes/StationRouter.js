c58f8dd601ef36ba11a24aee16049cb67213a448
Giap-Hong-Quan <giaphongquan2407@gmail.com>
2025-11-07 09:52:47 +0700

    hhh

--- Station_Management_Service/src/routes/StationRouter.js
+++ Station_Management_Service/src/routes/StationRouter.js
@@ -2,12 +2,13 @@ import express from "express";
 import { createStation,getAllStation,getStationById,deleteStation,updateStation,updateStationStatus,searchStations } from "../controllers/StationController.js"; 
 
 const router=express.Router();
-router.get('/',searchStations)
 
-router.post('/',createStation);
-router.get('/',getAllStation);
-router.get('/:id',getStationById);
-router.delete('/:id',deleteStation);
-router.put('/:id',updateStation);
-router.patch('/:id/status',updateStationStatus);
-export default router;
+router.post('/', createStation);
+// router.get('/search', searchStations); // GET /api/stations/search?keyword=...
+router.get('/', getAllStation); // GET /api/stations -> Lấy tất cả + count
+router.get('/:id', getStationById);
+router.delete('/:id', deleteStation);
+router.put('/:id', updateStation);
+router.patch('/:id/status', updateStationStatus);
+
+export default router;  
